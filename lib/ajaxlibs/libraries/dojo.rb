12982126d5bcab1ad660eb238658c82efabb72e6
Fabien Jakimowicz <fabien@jakimowicz.com>
2010-03-11 02:53:09 +0100

    add support for dojo and mootools

---
+++ lib/ajaxlibs/libraries/dojo.rb
@@ -0,0 +1,14 @@
+class Ajaxlibs::Library::Dojo < Ajaxlibs::Library
+  Versions = ["1.1.1",
+              "1.2.0",
+              "1.2.3",
+              "1.3.0",
+              "1.3.1",
+              "1.3.2",
+              "1.4.0",
+              "1.4.1"]
+  
+  def filename #:nodoc:
+    "dojo.xd.js.uncompressed"
+  end
+end
---
+++ lib/ajaxlibs/libraries/mootools.rb
@@ -0,0 +1,8 @@
+class Ajaxlibs::Library::Mootools < Ajaxlibs::Library
+  Versions = ["1.1.1",
+              "1.1.2",
+              "1.2.1",
+              "1.2.2",
+              "1.2.3",
+              "1.2.4"]
+end
