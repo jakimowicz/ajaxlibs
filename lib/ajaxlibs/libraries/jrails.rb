3f06114e6abc5317b292675c43d3849cdfeba65f
Fabien Jakimowicz <fabien@jakimowicz.com>
2010-04-28 14:55:30 +0200

    note

--- lib/ajaxlibs/libraries/jrails.rb
+++ lib/ajaxlibs/libraries/jrails.rb
@@ -1,5 +1,6 @@
 # JRails library, local support only.
 # This is only the javascript support of JRails, you must install JRails plugin to have a full support.
+# TODO : require jrails gem (or fail) if included
 class Ajaxlibs::Library::Jrails < Ajaxlibs::Library
   Versions = ['0.5.0']
 
